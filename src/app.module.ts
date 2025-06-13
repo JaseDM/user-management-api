3e8e1e6b4ca1d36b6d529d607940a26394d6563e
JaseDM <josemi.fotografo@gmail.com>
2025-06-13 13:15:19 +0200

    Insertadas variable de entorno en local y remoto

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,18 +24,32 @@ import { Role } from './roles/entities/role.entity';
     // Configuración de TypeORM
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USER'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities: [User, Role],
-        synchronize: configService.get('NODE_ENV') === 'development',
-        logging: false,
-        ssl: configService.get('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false,
-      }),
+      useFactory: (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+
+        return isProduction
+          ? {
+              type: 'postgres',
+              url: configService.get('DATABASE_URL'),
+              entities: [User, Role],
+              synchronize: false,
+              logging: false,
+              ssl: {
+                rejectUnauthorized: false,
+              },
+            }
+          : {
+              type: 'postgres',
+              host: configService.get('DATABASE_HOST'),
+              port: +configService.get('DATABASE_PORT'),
+              username: configService.get('DATABASE_USER'),
+              password: configService.get('DATABASE_PASSWORD'),
+              database: configService.get('DATABASE_NAME'),
+              entities: [User, Role],
+              synchronize: true, // ⚠️ Solo en desarrollo
+              logging: true,
+            };
+      },
       inject: [ConfigService],
     }),
 
